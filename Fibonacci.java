199b5d1eb113ced045c4997a5c23d3692df2aed3
Giulia R <84285314+RealGiulia@users.noreply.github.com>
2021-08-27 23:12:47 -0300

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,17 +6,17 @@ public class Fibonacci {
     public int n3;
 
 
-    public int soma(){
-            for (i=2;i<count;i++){
+    public int soma() {
+        for (i = 1; i < count; i++) {
+            if(n1 == 0){
+                n1 = n1 + 1;
+            }
+
             n3 = n1 + n2;
             n1 = n2;
             n2 = n3;
+
         }
-return  n3;
+        return n3;
     }
-
-
-
-
-
 }
